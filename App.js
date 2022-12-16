3c3c26588d45c0b60d8b6ef4b3bcdcc9fd5f824c
Natnael Gebremichael <natnael92460@gmail.com>
2022-12-16 22:55:42 +1300

    down graded react-native-area context to 3.3.2 .....

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
@@ -8,9 +9,9 @@ import { store } from "./store";
 export default function App() {
   return (
     <Provider store={store}>
-      <View>
+      <SafeAreaProvider>
         <HomeScreen />
-      </View>
+      </SafeAreaProvider>
     </Provider>
   );
 }
